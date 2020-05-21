7bf2a19d0dba97125be4b13fa08afc439fe111fe
wangzhen <zzuwangzhen@outlook.com>
2020-05-21 11:01:17 +0800

    1.0.0

---
+++ src/SuperDB.PostgreSQL/PostgreSQLFactory.cs
@@ -0,0 +1,66 @@
+﻿using Npgsql;
+
+using SuperDB.Config;
+
+using System.Data;
+
+namespace SuperDB.PostgreSQL
+{
+    public class PostgreSQLFactory : IDBFactory
+    {
+        private IDbConnection _Connection;
+        private IDbTransaction _Transaction;
+
+        public static PostgreSQLFactory Create()
+        {
+            return new PostgreSQLFactory();
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                if (_Connection == default)
+                {
+                    _Connection = new NpgsqlConnection(DBConfig.ConnectionString);
+                }
+                if (_Connection.State != ConnectionState.Open)
+                {
+                    _Connection.Open();
+                }
+                return _Connection;
+            }
+        }
+
+        public IDbTransaction Transaction
+        {
+            get
+            {
+                if (_Transaction == default)
+                {
+                    _Transaction = Connection.BeginTransaction();
+                }
+                return _Transaction;
+            }
+        }
+
+        public bool Commit()
+        {
+            Transaction.Commit();
+            return true;
+        }
+
+        public void Dispose()
+        {
+            _Connection?.Dispose();
+            _Connection = default;
+            _Transaction = default;
+        }
+
+        public bool Rollback()
+        {
+            Transaction.Rollback();
+            return false;
+        }
+    }
+}
